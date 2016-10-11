1eddbd58b2e1b1f8c112c32b52f7b525d78c6a7d
Ярослав Талеб < >
2016-10-11 22:50:24 +0300

    third commit

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -4,18 +4,24 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hello extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("greet")) {
-            List<String> programs = new ArrayList<String>();
+            ArrayList<String> programs = new ArrayList<String>();
 
             PackageManager packageManager = this.getApplicationContext().getPackageManager();
             List<ApplicationInfo> installedApplications = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
 
-            ArrayList <ApplicationInfo> applicationInfos = new ArrayList<>();
+            ArrayList <ApplicationInfo> applicationInfos = new ArrayList<ApplicationInfo>();
 
             for (ApplicationInfo installedApplication : installedApplications) {
                 if (packageManager.getLaunchIntentForPackage(installedApplication.packageName) != null){
@@ -25,12 +31,9 @@ public class Hello extends CordovaPlugin {
 
             for (ApplicationInfo applicationInfo : applicationInfos) {
                 programs.add(applicationInfo.loadLabel(packageManager).toString());
-                programs.add("\n");
             }
 
-//            System.out.println(programs);
-
-            callbackContext.success(programs.toString);
+            callbackContext.success(programs.toString());
 
             return true;
 
